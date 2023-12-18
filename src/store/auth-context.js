befaf890a44afbe69bfd2d5d626c4ebeedd42a48
Mona Khorasani <monakhrsn@gmail.com>
2023-12-18 12:27:29 +0100

    Refactor, now with one central context and one central state managment

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    //just for IDE
+    onlogout: () => {},
+    onLogin: (email, pasaword) => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(()=> {
+        const storedUserLoggedInInfo = localStorage.getItem('setIsLoggedIn');
+    
+        if(storedUserLoggedInInfo === '1') {
+          setIsLoggedIn(true);
+        }
+      }, [])
+
+    const logoutHandler = () => {
+        localStorage.removeItem('isLoggedIn')
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler = () => {
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    };
+
+    return <AuthContext.Provider value={{isLoggedIn: isLoggedIn, onlogout: logoutHandler, onLogin: loginHandler}}>{props.children}</AuthContext.Provider>
+};
 
 export default AuthContext;
